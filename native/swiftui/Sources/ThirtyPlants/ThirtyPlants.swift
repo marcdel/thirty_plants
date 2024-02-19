b7465e54fb65c1f02ec0272ac3bf67ba3ad8fafd
Marc Delagrammatikas <marc@mechanical-orchard.com>
2024-02-18 22:54:34 -0800

    generate liveview-native files with mix lvn.install

---
+++ native/swiftui/Sources/ThirtyPlants/ThirtyPlants.swift
@@ -0,0 +1,15 @@
+//
+//  ThirtyPlants.swift
+//  ThirtyPlants
+//
+
+import SwiftUI
+
+@main
+struct ThirtyPlants: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
